5a9ed9acd81b34598e2a2a4ddb3f71a56925ad1a
GatoArtStudio <hackhcctecno@gmail.com>
2024-08-11 10:03:46 -0500

    Agregando velocidad a las botas de warden

--- src/main/java/net/mcreator/klv/procedures/BotasWardenProcedureProcedure.java
+++ src/main/java/net/mcreator/klv/procedures/BotasWardenProcedureProcedure.java
@@ -1,11 +1,17 @@
 package net.mcreator.klv.procedures;
 
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 
 public class BotasWardenProcedureProcedure {
 	public static void execute(Entity entity) {
 		if (entity == null)
 			return;
+		if (entity instanceof LivingEntity _entity && !_entity.level.isClientSide()) {
+			_entity.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 70, 0, false, false));
+		}
 		if (entity.isShiftKeyDown()) {
 			entity.clearFire();
 		}
