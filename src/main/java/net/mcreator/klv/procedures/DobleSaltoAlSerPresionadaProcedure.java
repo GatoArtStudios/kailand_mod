b9c4b6d032ccfe0e8b44852c75d48292ad73978e
GatoArtStudio <hackhcctecno@gmail.com>
2024-09-23 17:38:58 -0500

    Se a agregado nuevamente el bloque velox para evitar crash al cargar chunks con el mineral

--- src/main/java/net/mcreator/klv/procedures/DobleSaltoAlSerPresionadaProcedure.java
+++ src/main/java/net/mcreator/klv/procedures/DobleSaltoAlSerPresionadaProcedure.java
@@ -34,71 +34,81 @@ public class DobleSaltoAlSerPresionadaProcedure {
 			// Verifica si esta montado en alguna entidad o vehiculo y no le permite usar la habilidad
 			if (entity.isPassenger() || entity.isVehicle()) return;
 
-			if ((new Object() {
-				public boolean checkGamemode(Entity _ent) {
-					if (_ent instanceof ServerPlayer _serverPlayer) {
-						return _serverPlayer.gameMode.getGameModeForPlayer() == GameType.SURVIVAL;
-					} else if (_ent.level.isClientSide() && _ent instanceof Player _player) {
-						return Minecraft.getInstance().getConnection().getPlayerInfo(_player.getGameProfile().getId()) != null
-								&& Minecraft.getInstance().getConnection().getPlayerInfo(_player.getGameProfile().getId()).getGameMode() == GameType.SURVIVAL;
+			// verificamos si el jugador esta en el suelo
+			if (entity.isOnGround()) {
+				entity.getPersistentData().putInt("dobleSalto", 2);
+			}
+
+			int saltosRestantes = entity.getPersistentData().getInt("dobleSalto");
+
+			if (saltosRestantes > 0 ) {
+				entity.getPersistentData().putInt("dobleSalto", saltosRestantes - 1);
+				if ((new Object() {
+					public boolean checkGamemode(Entity _ent) {
+						if (_ent instanceof ServerPlayer _serverPlayer) {
+							return _serverPlayer.gameMode.getGameModeForPlayer() == GameType.SURVIVAL;
+						} else if (_ent.level.isClientSide() && _ent instanceof Player _player) {
+							return Minecraft.getInstance().getConnection().getPlayerInfo(_player.getGameProfile().getId()) != null
+									&& Minecraft.getInstance().getConnection().getPlayerInfo(_player.getGameProfile().getId()).getGameMode() == GameType.SURVIVAL;
+						}
+						return false;
 					}
-					return false;
-				}
-			}.checkGamemode(entity) || new Object() {
-				public boolean checkGamemode(Entity _ent) {
-					if (_ent instanceof ServerPlayer _serverPlayer) {
-						return _serverPlayer.gameMode.getGameModeForPlayer() == GameType.ADVENTURE;
-					} else if (_ent.level.isClientSide() && _ent instanceof Player _player) {
-						return Minecraft.getInstance().getConnection().getPlayerInfo(_player.getGameProfile().getId()) != null
-								&& Minecraft.getInstance().getConnection().getPlayerInfo(_player.getGameProfile().getId()).getGameMode() == GameType.ADVENTURE;
+				}.checkGamemode(entity) || new Object() {
+					public boolean checkGamemode(Entity _ent) {
+						if (_ent instanceof ServerPlayer _serverPlayer) {
+							return _serverPlayer.gameMode.getGameModeForPlayer() == GameType.ADVENTURE;
+						} else if (_ent.level.isClientSide() && _ent instanceof Player _player) {
+							return Minecraft.getInstance().getConnection().getPlayerInfo(_player.getGameProfile().getId()) != null
+									&& Minecraft.getInstance().getConnection().getPlayerInfo(_player.getGameProfile().getId()).getGameMode() == GameType.ADVENTURE;
+						}
+						return false;
 					}
-					return false;
-				}
-			}.checkGamemode(entity)) && !entity.isOnGround() && KlvModVariables.MapVariables.get(world).Temporizador > 0) {
-				KlvModVariables.MapVariables.get(world).Temporizador = KlvModVariables.MapVariables.get(world).Temporizador - 1;
-				KlvModVariables.MapVariables.get(world).syncData(world);
-				if (world instanceof ServerLevel _level)
-					_level.sendParticles(ParticleTypes.SPIT, x, y, z, 15, 1, 1, 1, 0.5);
-				if (world instanceof Level _level) {
-					if (!_level.isClientSide()) {
-						_level.playSound(null, new BlockPos(x, y, z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.moss_carpet.break")), SoundSource.PLAYERS, 1, 1);
+				}.checkGamemode(entity)) && !entity.isOnGround() && KlvModVariables.MapVariables.get(world).Temporizador > 0) {
+					KlvModVariables.MapVariables.get(world).Temporizador = KlvModVariables.MapVariables.get(world).Temporizador - 1;
+					KlvModVariables.MapVariables.get(world).syncData(world);
+					if (world instanceof ServerLevel _level)
+						_level.sendParticles(ParticleTypes.SPIT, x, y, z, 15, 1, 1, 1, 0.5);
+					if (world instanceof Level _level) {
+						if (!_level.isClientSide()) {
+							_level.playSound(null, new BlockPos(x, y, z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.moss_carpet.break")), SoundSource.PLAYERS, 1, 1);
+						} else {
+							_level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.moss_carpet.break")), SoundSource.PLAYERS, 1, 1, false);
+						}
+					}
+					if ((entity instanceof LivingEntity _livEnt ? _livEnt.hasEffect(MobEffects.JUMP) : false)
+							&& (entity instanceof LivingEntity _livEnt && _livEnt.hasEffect(MobEffects.JUMP) ? _livEnt.getEffect(MobEffects.JUMP).getAmplifier() : 0) == 0) {
+						entity.setDeltaMovement(new Vec3((entity.getDeltaMovement().x()), 0.75, (entity.getDeltaMovement().z())));
+						if (world instanceof ServerLevel _level)
+							_level.sendParticles(ParticleTypes.SPIT, x, y, z, 15, 1, 1, 1, 0.5);
+						if (world instanceof Level _level) {
+							if (!_level.isClientSide()) {
+								_level.playSound(null, new BlockPos(x, y, z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.moss_carpet.break")), SoundSource.PLAYERS, 1, 1);
+							} else {
+								_level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.moss_carpet.break")), SoundSource.PLAYERS, 1, 1, false);
+							}
+						}
+					} else if ((entity instanceof LivingEntity _livEnt ? _livEnt.hasEffect(MobEffects.JUMP) : false)
+							&& (entity instanceof LivingEntity _livEnt && _livEnt.hasEffect(MobEffects.JUMP) ? _livEnt.getEffect(MobEffects.JUMP).getAmplifier() : 0) == 1) {
+						entity.setDeltaMovement(new Vec3((entity.getDeltaMovement().x()), 1, (entity.getDeltaMovement().z())));
+						if (world instanceof ServerLevel _level)
+							_level.sendParticles(ParticleTypes.SPIT, x, y, z, 15, 1, 1, 1, 0.5);
+						if (world instanceof Level _level) {
+							if (!_level.isClientSide()) {
+								_level.playSound(null, new BlockPos(x, y, z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.moss_carpet.break")), SoundSource.PLAYERS, 1, 1);
+							} else {
+								_level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.moss_carpet.break")), SoundSource.PLAYERS, 1, 1, false);
+							}
+						}
 					} else {
-						_level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.moss_carpet.break")), SoundSource.PLAYERS, 1, 1, false);
-					}
-				}
-				if ((entity instanceof LivingEntity _livEnt ? _livEnt.hasEffect(MobEffects.JUMP) : false)
-						&& (entity instanceof LivingEntity _livEnt && _livEnt.hasEffect(MobEffects.JUMP) ? _livEnt.getEffect(MobEffects.JUMP).getAmplifier() : 0) == 0) {
-					entity.setDeltaMovement(new Vec3((entity.getDeltaMovement().x()), 0.75, (entity.getDeltaMovement().z())));
-					if (world instanceof ServerLevel _level)
-						_level.sendParticles(ParticleTypes.SPIT, x, y, z, 15, 1, 1, 1, 0.5);
-					if (world instanceof Level _level) {
-						if (!_level.isClientSide()) {
-							_level.playSound(null, new BlockPos(x, y, z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.moss_carpet.break")), SoundSource.PLAYERS, 1, 1);
-						} else {
-							_level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.moss_carpet.break")), SoundSource.PLAYERS, 1, 1, false);
-						}
-					}
-				} else if ((entity instanceof LivingEntity _livEnt ? _livEnt.hasEffect(MobEffects.JUMP) : false)
-						&& (entity instanceof LivingEntity _livEnt && _livEnt.hasEffect(MobEffects.JUMP) ? _livEnt.getEffect(MobEffects.JUMP).getAmplifier() : 0) == 1) {
-					entity.setDeltaMovement(new Vec3((entity.getDeltaMovement().x()), 1, (entity.getDeltaMovement().z())));
-					if (world instanceof ServerLevel _level)
-						_level.sendParticles(ParticleTypes.SPIT, x, y, z, 15, 1, 1, 1, 0.5);
-					if (world instanceof Level _level) {
-						if (!_level.isClientSide()) {
-							_level.playSound(null, new BlockPos(x, y, z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.moss_carpet.break")), SoundSource.PLAYERS, 1, 1);
-						} else {
-							_level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.moss_carpet.break")), SoundSource.PLAYERS, 1, 1, false);
-						}
-					}
-				} else {
-					entity.setDeltaMovement(new Vec3((entity.getDeltaMovement().x()), 0.5, (entity.getDeltaMovement().z())));
-					if (world instanceof ServerLevel _level)
-						_level.sendParticles(ParticleTypes.SPIT, x, y, z, 15, 1, 1, 1, 0.5);
-					if (world instanceof Level _level) {
-						if (!_level.isClientSide()) {
-							_level.playSound(null, new BlockPos(x, y, z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.moss_carpet.break")), SoundSource.PLAYERS, 1, 1);
-						} else {
-							_level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.moss_carpet.break")), SoundSource.PLAYERS, 1, 1, false);
+						entity.setDeltaMovement(new Vec3((entity.getDeltaMovement().x()), 0.5, (entity.getDeltaMovement().z())));
+						if (world instanceof ServerLevel _level)
+							_level.sendParticles(ParticleTypes.SPIT, x, y, z, 15, 1, 1, 1, 0.5);
+						if (world instanceof Level _level) {
+							if (!_level.isClientSide()) {
+								_level.playSound(null, new BlockPos(x, y, z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.moss_carpet.break")), SoundSource.PLAYERS, 1, 1);
+							} else {
+								_level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.moss_carpet.break")), SoundSource.PLAYERS, 1, 1, false);
+							}
 						}
 					}
 				}
